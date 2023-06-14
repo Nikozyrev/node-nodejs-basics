a0ca5e5a1f406bac6dd76ba781c960a211f70938
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-14 12:07:06 +0300

    feat: implement fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { rename as fsRename } from 'fs/promises';
+import { hasAccess } from '../../helpers/hasAccess.js';
+
 const rename = async () => {
-    // Write your code here 
+  const srcPath = new URL(`./files/wrongFilename.txt`, import.meta.url);
+  const destPath = new URL(`./files/properFilename.md`, import.meta.url);
+
+  const isSrcPathExists = await hasAccess(srcPath);
+  const isDestPathExists = await hasAccess(destPath);
+
+  if (!isSrcPathExists || isDestPathExists)
+    throw new Error('FS operation failed');
+
+  return await fsRename(srcPath, destPath);
 };
 
 await rename();
