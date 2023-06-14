43c0453d385ca092c7239c958de83d3f3c4fdc3c
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-14 12:26:03 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from 'fs/promises';
+import { hasAccess } from '../../helpers/hasAccess.js';
+
 const list = async () => {
-    // Write your code here 
+  const path = new URL(`./files`, import.meta.url);
+
+  const isPathExists = await hasAccess(path);
+
+  if (!isPathExists) throw new Error('FS operation failed');
+
+  const files = await readdir(path);
+
+  console.log(files.join('\n'));
 };
 
 await list();
