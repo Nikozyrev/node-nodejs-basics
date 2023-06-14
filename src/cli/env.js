005170d351b8a9751d0eb32fd9856c4fe9c23098
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-14 12:57:36 +0300

    feat: implement cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const rssVars = Object.entries(process.env).filter(([key]) =>
+    key.startsWith(prefix)
+  );
+
+  if (!rssVars.length) {
+    console.log(`No env variables starts with ${prefix}`);
+  }
+
+  const result = rssVars.map(([key, value]) => `${key}=${value}`).join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
