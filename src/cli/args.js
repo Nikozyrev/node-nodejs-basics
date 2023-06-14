53bc2725f9385916643d258c25f824f63e23e59f
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-14 17:49:20 +0300

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = Array.from(process.argv).slice(2);
+  const argsPaired = args.reduce(
+    (acc, val, i, arr) => (i % 2 === 0 ? [...acc, [val, arr[i + 1]]] : acc),
+    []
+  );
+  const result = argsPaired
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
