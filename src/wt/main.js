ee7611650dead7fe837224f67a68a1e0fb2a8ebf
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-17 15:48:37 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const executeWorker = (workerData) => {
+  const path = new URL('./worker.js', import.meta.url);
+  const worker = new Worker(path, { workerData });
+  return new Promise((resolve) => {
+    worker.on('message', ({ result }) =>
+      resolve({ status: 'resolved', data: result })
+    );
+    worker.on('error', () => resolve({ status: 'error', data: null }));
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const startNum = 10;
+  const cpusArr = cpus();
+  const result = await Promise.allSettled(
+    cpusArr.map((_, i) => executeWorker({ n: startNum + i }))
+  );
+  console.log(result.map(({ value }) => value));
 };
 
 await performCalculations();
